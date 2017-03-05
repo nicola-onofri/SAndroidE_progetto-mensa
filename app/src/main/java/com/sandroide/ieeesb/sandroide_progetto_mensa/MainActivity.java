b189cac0c35b686e0c51abca6471665c6fd344f1
nicola-onofri <nicola.onofri@gmail.com>
2017-03-05 12:14:27 +0100

    Applied SAndroidE empty template

---
+++ app/src/main/java/com/sandroide/ieeesb/sandroide_progetto_mensa/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sandroide.ieeesb.sandroide_progetto_mensa;
+
+import android.os.Bundle;
+
+import it.unibs.sandroide.lib.activities.SandroideBaseActivity;
+
+public class MainActivity extends SandroideBaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
